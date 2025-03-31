3e81bd59b5978288068597fd73b4f375da9ad7a2
Henry-Hiles <henry@henryhiles.com>
2025-03-30 20:54:01 -0400

    Should be done for now

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig, passthroughImageService } from "astro/config"
-import icon from "astro-icon"
 
 export default defineConfig({
     trailingSlash: "never",
-    integrations: [icon()],
     build: { format: "directory" },
     image: { service: passthroughImageService() },
 })
