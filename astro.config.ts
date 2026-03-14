70a196bf37bb1ad50edcf04d895dbafef29c06e3
Henry-Hiles <henry@henryhiles.com>
2026-03-14 16:34:25 -0400

    add privacy policy

--- astro.config.ts
+++ astro.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
                     autogenerate: { directory: "services" },
                 },
                 { slug: "code" },
+                { slug: "privacy-policy" },
                 { slug: "donate" },
             ],
 
